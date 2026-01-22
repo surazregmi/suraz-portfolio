04ddf02d8ea42b4a057bb3c886aafaf68d323871
waynezhou <hyakoste@gmail.com>
2026-01-22 15:32:31 +1100

    change blog title.

--- src/data/blogs.tsx
+++ src/data/blogs.tsx
@@ -2,7 +2,7 @@
 export const blogs = [
   {
     slug: "understanding-nodejs",
-    title: "Understanding Node.js Fundamentals",
+    title: "From Single Thread to Event Loop: How Node.js Handles Async Code",
     url: "https://raw.githubusercontent.com/surazregmi/dev-blogs/refs/heads/master/Main/understanding-nodejs-core-concepts.md",
     image:
       "https://images.unsplash.com/photo-1484417894907-623942c8ee29?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
