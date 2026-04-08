78b270ae6305cba72df46cf4bcf3c4eea2442053
waynezhou <hyakoste@gmail.com>
2026-04-08 16:24:30 +1000

    fix slug

--- src/data/blogs.tsx
+++ src/data/blogs.tsx
@@ -8,8 +8,8 @@ export const blogs = [
       "https://images.unsplash.com/photo-1484417894907-623942c8ee29?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    slug: "understanding-nodejs",
-    title: "From Single Thread to Event Loop: How Node.js Handles Async Code",
+    slug: "understanding-network",
+    title: "Networking Fundamentals You MUST Know Before Learning AWS",
     url: "https://raw.githubusercontent.com/surazregmi/dev-blogs/refs/heads/master/Main/networking-fundamentals-for-aws.md",
     image:
       "https://img.freepik.com/free-photo/network-people-digital-world_9975-23272.jpg?semt=ais_hybrid&w=740&q=80",
